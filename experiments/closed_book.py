d06e35486594bd5e148e373a8af02d4a3f896283
Irene Papadopoulou <irenepap3@gmail.com>
2022-09-29 17:53:53 +0200

    Added no_repeat_ngram_size=3 to closed book exp.

--- experiments/closed_book.py
+++ experiments/closed_book.py
@@ -2,14 +2,13 @@ from datasets import load_dataset
 from transformers import AutoModelForSeq2SeqLM, T5TokenizerFast
 import json
 
-MODEL_PATH = "t5_finetuned/checkpoint-25000/"
-JSON_FILENAME = "t5_ft_closed_book_predictions.json"
+JSON_FILENAME = "t5_ft_closed_book_predictions_ngram_3.json"
 BATCH_SIZE = 64
 pred_dict = {}
 
 dataset = load_dataset("din0s/asqa")
 tokenizer = T5TokenizerFast.from_pretrained("google/t5-large-ssm-nq")
-model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_PATH)
+model = AutoModelForSeq2SeqLM.from_pretrained("irenepap/t5-small-asqa-cb")
 
 print("Extracting T5 predictions..")
 for i in range(0, len(dataset["dev"]), BATCH_SIZE):
@@ -22,8 +21,10 @@ for i in range(0, len(dataset["dev"]), BATCH_SIZE):
     gen_outputs = model.generate(
         inputs["input_ids"], 
         attention_mask=inputs["attention_mask"], 
-        max_length=100, num_beams=5, 
-        early_stopping=True)
+        no_repeat_ngram_size=3,
+        max_length=100, 
+        num_beams=5, 
+        early_stopping=True,)
 
     answers = tokenizer.batch_decode(gen_outputs, skip_special_tokens=True)
 
