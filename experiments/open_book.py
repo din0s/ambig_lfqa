009f1372ff3eac5aa4e714035da55e363a0d773c
Irene Papadopoulou <irenepap3@gmail.com>
2022-09-30 12:26:25 +0200

    Added open book experiment

--- experiments/open_book.py
+++ experiments/open_book.py
@@ -5,14 +5,20 @@ import torch
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-JSON_FILENAME = "t5_ft_closed_book_predictions_ngram_3.json"
+JSON_FILENAME = "t5_ft_ob_dpr_predictions.json"
 BATCH_SIZE = 64
 pred_dict = {}
 
 dataset = load_dataset("din0s/asqa")
-tokenizer = T5TokenizerFast.from_pretrained("google/t5-large-ssm-nq")
-model = AutoModelForSeq2SeqLM.from_pretrained("irenepap/t5-small-asqa-cb")
+tokenizer = T5TokenizerFast.from_pretrained("google/t5-small-ssm-nq")
+model = AutoModelForSeq2SeqLM.from_pretrained("din0s/t5-small-asqa-ob")
 model = model.to(device)
+
+# Load passages (only for open book)
+dpr_path = "./passages/dpr_passages.json"
+
+with open(dpr_path, "r") as f:
+    passages = json.load(f)
 
 print("Extracting T5 predictions..")
 for i in range(0, len(dataset["dev"]), BATCH_SIZE):
@@ -20,8 +26,12 @@ for i in range(0, len(dataset["dev"]), BATCH_SIZE):
     print("Batch: ", i)
     batch = dataset["dev"][i:i+BATCH_SIZE]
     questions = batch['ambiguous_question']
+    context = [passages[i][0] for i in batch['sample_id']]
+
+    q_and_a = [f"question: {q} context: {c}" for q, c in zip(questions, context)]
     
-    inputs = tokenizer(questions, return_tensors="pt", padding=True).to(device)
+    inputs = tokenizer(q_and_a, return_tensors="pt", padding=True).to(device)
+    
     gen_outputs = model.generate(
         inputs["input_ids"], 
         attention_mask=inputs["attention_mask"], 
