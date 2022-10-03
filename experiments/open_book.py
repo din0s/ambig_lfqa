cf131c3308b81b51403ad0a5ca660786e9bebfad
Irene Papadopoulou <irenepap3@gmail.com>
2022-10-03 02:21:36 +0200

    Changed rouge calculation, updated open book experiment.

--- experiments/open_book.py
+++ experiments/open_book.py
@@ -10,9 +10,10 @@ BATCH_SIZE = 64
 pred_dict = {}
 
 dataset = load_dataset("din0s/asqa")
-tokenizer = T5TokenizerFast.from_pretrained("google/t5-small-ssm-nq")
-model = AutoModelForSeq2SeqLM.from_pretrained("din0s/t5-small-asqa-ob")
+tokenizer = T5TokenizerFast.from_pretrained("t5-base")
+model = AutoModelForSeq2SeqLM.from_pretrained("irenepap/t5-base-asqa-ob")
 model = model.to(device)
+model.eval()
 
 # Load passages (only for open book)
 dpr_path = "./passages/dpr_passages.json"
@@ -20,30 +21,31 @@ dpr_path = "./passages/dpr_passages.json"
 with open(dpr_path, "r") as f:
     passages = json.load(f)
 
-print("Extracting T5 predictions..")
-for i in range(0, len(dataset["dev"]), BATCH_SIZE):
-    
-    print("Batch: ", i)
-    batch = dataset["dev"][i:i+BATCH_SIZE]
-    questions = batch['ambiguous_question']
-    context = [passages[i][0] for i in batch['sample_id']]
+with torch.no_grad():
+    print("Extracting T5 predictions..")
+    for i in range(0, len(dataset["dev"]), BATCH_SIZE):
+        
+        print("Batch: ", i)
+        batch = dataset["dev"][i:i+BATCH_SIZE]
+        questions = batch['ambiguous_question']
+        context = [passages[i][0] for i in batch['sample_id']]
 
-    q_and_a = [f"question: {q} context: {c}" for q, c in zip(questions, context)]
-    
-    inputs = tokenizer(q_and_a, return_tensors="pt", padding=True).to(device)
-    
-    gen_outputs = model.generate(
-        inputs["input_ids"], 
-        attention_mask=inputs["attention_mask"], 
-        no_repeat_ngram_size=3,
-        max_length=100, 
-        num_beams=5, 
-        early_stopping=True,)
+        q_and_a = [f"question: {q} context: {c}" for q, c in zip(questions, context)]
+        
+        inputs = tokenizer(q_and_a, return_tensors="pt", padding=True).to(device)
+        
+        gen_outputs = model.generate(
+            inputs["input_ids"], 
+            attention_mask=inputs["attention_mask"], 
+            no_repeat_ngram_size=3,
+            max_length=100, 
+            num_beams=5, 
+            early_stopping=True,)
 
-    answers = tokenizer.batch_decode(gen_outputs, skip_special_tokens=True)
+        answers = tokenizer.batch_decode(gen_outputs, skip_special_tokens=True)
 
-    id_ans_list = list(zip(batch['sample_id'], answers))
-    pred_dict.update({str(sample_id) : ans for sample_id, ans in id_ans_list})
+        id_ans_list = list(zip(batch['sample_id'], answers))
+        pred_dict.update({str(sample_id) : ans for sample_id, ans in id_ans_list})
 
 print("Saving predictions to json file..")
 with open(JSON_FILENAME, 'w') as fp:
--- finetune_T5.py
+++ finetune_T5.py
@@ -1,21 +1,21 @@
 from datasets import load_dataset
 from transformers import AutoModelForSeq2SeqLM, T5TokenizerFast, Seq2SeqTrainingArguments, Trainer, set_seed, DataCollatorForSeq2Seq, EarlyStoppingCallback
-import evaluate
+from scoring import _rouge_calculation as rouge
 import torch
 import json
 
 set_seed(42)
-model_name = "google/t5-small-ssm-nq"
-train_batch_size = 8
+model_name = "t5-base"
+train_batch_size = 10
 eval_batch_size = 8
 # name the model to push to hub
-ft_model_name = "t5-small-asqa-ob-bm25@5"
+ft_model_name = "t5-base-asqa-ob"
 
 # Load passages (only for open book)
-bm25_path = "./passages/bm25_passages_all.json"
+# bm25_path = "./passages/bm25_passages_all.json"
 
-with open(bm25_path, "r") as f:
-    passages = json.load(f)
+# with open(bm25_path, "r") as f:
+#     passages = json.load(f)
 
 def has_context(example):
     context = [i["context"] for i in example["qa_pairs"]]
@@ -26,13 +26,13 @@ def has_context(example):
 
 def tokenize_function(example):
     # With ASQA context
-    # context = [i["context"] for i in example["qa_pairs"]]
-    # context = list(filter(("No context provided").__ne__, context))
-    # context = ' | '.join(context)
+    context = [i["context"] for i in example["qa_pairs"]]
+    context = list(filter(("No context provided").__ne__, context))
+    context = ' | '.join(context)
 
     # With retrieved context
-    sample_id = example['sample_id']
-    context = ' | '.join(passages[sample_id][:3])
+    # sample_id = example['sample_id']
+    # context = ' | '.join(passages[sample_id][:3])
     
     question_tokenized = tokenizer(f"question: {example['ambiguous_question']} context: {context}", truncation=True, max_length=512)
     example['input_ids'] = question_tokenized['input_ids']
@@ -52,7 +52,7 @@ def compute_metrics(pred):
     preds_ids[preds_ids == -100] = tokenizer.pad_token_id
     labels = tokenizer.batch_decode(labels_ids, skip_special_tokens=True)
     preds = tokenizer.batch_decode(preds_ids, skip_special_tokens=True)
-    return rouge.compute(predictions = preds, references = labels)
+    return rouge(hypothesis= labels, references1 = preds)
 
 def get_argmax(logits, _):
     preds_ids = torch.cat([torch.argmax(i, dim=-1) for i in logits], dim=1)
@@ -68,9 +68,9 @@ dataset = dataset.remove_columns(col_remove)
 tokenizer = T5TokenizerFast.from_pretrained(model_name)
 
 # With ASQA context
-# tokenized_datasets = dataset.filter(has_context).map(tokenize_function, remove_columns=["annotations"])
+tokenized_datasets = dataset.filter(has_context).map(tokenize_function, remove_columns=["annotations"])
 # With BM25 passages
-tokenized_datasets = dataset.map(tokenize_function, remove_columns=["annotations"])
+# tokenized_datasets = dataset.map(tokenize_function, remove_columns=["annotations"])
 
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
@@ -81,19 +81,15 @@ training_args = Seq2SeqTrainingArguments(
     group_by_length=True,
     per_device_train_batch_size=train_batch_size, 
     per_device_eval_batch_size=eval_batch_size, 
-    # eval_accumulation_steps=8,
     num_train_epochs=50,
     save_strategy="epoch",
     save_total_limit=1,
     report_to="wandb",
-    learning_rate=5e-4,
-    metric_for_best_model="rougeLsum",
+    learning_rate=1e-5,
+    metric_for_best_model="loss",
     push_to_hub=True,
     load_best_model_at_end = True,
 )
-
-# Load metric
-rouge = evaluate.load('rouge')
 
 trainer = Trainer(
     model=model,
